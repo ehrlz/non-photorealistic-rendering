3d2ead07604838a48363a6837d6d1292538966cc
Elías Herrero <elias.herrero.lazaro@alumnos.upm.es>
2024-05-24 01:16:56 +0200

    Add: separated file for gui

--- src/GPO_aux.cpp
+++ src/GPO_aux.cpp
@@ -14,11 +14,6 @@
 #include <assimp/postprocess.h>
 #include <iostream>
 
-//imgui
-#include "imgui.h"
-#include "backends/imgui_impl_glfw.h"
-#include "backends/imgui_impl_opengl3.h"
-
 ////////////////////   
 
 extern int ANCHO, ALTO;
@@ -75,38 +70,6 @@ void load_Opengl(void)
 	printf("---------------------------------------------\n");
 	return;
 }
-
-
-void setupImGui(GLFWwindow* window) {
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    ImGui::StyleColorsDark();
-
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init("#version 130");
-}
-
-void renderImGui(void) {
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
-
-    // Ventanas ImGui
-    ImGui::Begin("Hello, world!");
-    ImGui::Text("This is some useful text.");
-    ImGui::End();
-
-    ImGui::Render();
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-}
-
-void terminateImGui(void) {
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-}
-
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // CARGA, COMPILACION Y LINKADO DE LOS SHADERS
