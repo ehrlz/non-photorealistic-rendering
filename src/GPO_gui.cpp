edda535371a9fbc2ebe88c8d91522c501f425ff6
Elías Herrero <elias.herrero.lazaro@alumnos.upm.es>
2024-05-24 17:31:38 +0200

    Modify: shader and texture options in other window

--- src/GPO_gui.cpp
+++ src/GPO_gui.cpp
@@ -19,8 +19,15 @@ void renderImGui(int *scene_flag, int *model_flag, int *render_texture, int *col
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
-    // Ventanas ImGui
-    ImGui::Begin("App selector");
+    // First window in top right corner (only first use, can be moved)
+    int width_window = 255;
+    int height_window = 175;
+    const ImGuiViewport* main_viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(ImVec2(main_viewport->WorkPos.x + (1200 - width_window), main_viewport->WorkPos.y)); //ImGuiCond_FirstUseEver for only first use
+    ImGui::SetNextWindowSize(ImVec2(width_window, height_window));
+    
+    // First window: shader and model selector
+    ImGui::Begin("Scenes", NULL, ImGuiWindowFlags_NoMove || ImGuiWindowFlags_NoResize);
     ImGui::SeparatorText("Shaders");
     ImGui::RadioButton("Pixel1", scene_flag, PIXEL1); ImGui::SameLine();
     ImGui::RadioButton("Pixel2", scene_flag, PIXEL2);
@@ -36,30 +43,39 @@ void renderImGui(int *scene_flag, int *model_flag, int *render_texture, int *col
     ImGui::RadioButton("Fountain-Ball", model_flag, BALL);
     ImGui::RadioButton("Helmet", model_flag, HELMET); ImGui::SameLine();
     ImGui::RadioButton("Cat", model_flag, CAT);
+    ImGui::End();
 
-    ImGui::SeparatorText("Options");
+    // Second window pos: top left corner
+    width_window = 300;
+    height_window = 350;
+    ImGui::SetNextWindowPos(ImVec2(main_viewport->WorkPos.x, main_viewport->WorkPos.y));
+    ImGui::SetNextWindowSize(ImVec2(300, 350));
+
+    // Second window: Shader and texture options
+    ImGui::Begin("Scene options", NULL, ImGuiWindowFlags_NoMove && ImGuiWindowFlags_NoResize);
     if(*scene_flag != PIXEL1 && *scene_flag != PIXEL2){ // TODO PIXEL
-        if(*model_flag != BALL){
-            ImGui::Checkbox("Render textures", (bool *)render_texture); // Fountain ball doesn't have texture,
-        }
         if(*render_texture == 0){ // color shouldn't be defined with active texture
             float col[3] = {model_color->x,model_color->y,model_color->z};
+            ImGui::ColorPicker3("Model color", col, ImGuiColorEditFlags_PickerHueWheel | ImGuiColorEditFlags_NoSidePreview | ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoAlpha);
             ImGui::ColorEdit3("Model color", col);
             model_color->x = col[0];
             model_color->y = col[1];
             model_color->z = col[2];
         }
-    } 
-        
+        if(*model_flag != BALL){
+            ImGui::Checkbox("Render textures", (bool *)render_texture); // Fountain ball doesn't have texture,
+        } else {
+            ImGui::Dummy(ImVec2(0.0f, 20.0f)); // vertical spacing
+        }
+    }
 
+    ImGui::SeparatorText("Shader params");
     if(*scene_flag == TOON){
-        ImGui::SeparatorText("Toon shading params");
         ImGui::SliderFloat("Border", toon_border, 0.0f, 0.5f, "ratio = %.05f");
         ImGui::SliderInt("Color levels", color_levels, 3, 7);
     }
-        
     ImGui::End();
-
+        
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
